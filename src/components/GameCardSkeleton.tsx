1cc8a922e98dbfe2bcf5c7013d63406142c5fee1
LINTlikr <LINTlikr@users.noreply.github.com>
2023-12-12 15:04:20 -0800

    making skeleton look more like empty cards

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,13 +1,28 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Skeleton,
+  SkeletonText,
+  Stack,
+} from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton height="200px">
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Skeleton>
+    <Card width="300px" height="320px">
+      <Stack>
+        <Skeleton
+          height="200px"
+          borderTopLeftRadius={10}
+          borderTopRightRadius={10}
+        ></Skeleton>
+        <SkeletonText
+          mt="2"
+          noOfLines={3}
+          spacing="3"
+          skeletonHeight="3"
+          padding="10px"
+        />
+      </Stack>
     </Card>
   );
 };
